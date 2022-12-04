2a718caa30a19897fa455f62bd42718dd96430c7
Mutasim Sweileh <mutasim.sweileh@gmail.com>
2022-12-04 12:48:38 +0200

    modified:   gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,6 @@ module.exports = {
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@MutasimSweileh',
   },
-  flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
